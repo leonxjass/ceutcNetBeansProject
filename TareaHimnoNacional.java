77a4dc12e258c0a87b9a5e756987beb7c0780255
Anderson Leon <115023863+leonxjass@users.noreply.github.com>
2023-07-17 11:16:55 -0600

    lab 1 inicio de laboratorio - imprimiendo datos
    Practica de interfaz y sintaxis inicial, imprimimos datos del homno nacional, Laboratorio 1

---
+++ TareaHimnoNacional.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareahimnonacional;
+
+/**
+ *
+ * @author tguca-L01
+ */
+public class TareaHimnoNacional {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Practicando imprimir en pantalla himno nacional - lab 1
+        
+        System.out.println("CORO\nTu bandera es un lampo del cielo"
+                + "\nPor un bloque de nieve cruzado"
+                + "\nY se ven en su fondo cruzado"
+                + "\nCinco estrellas de palido azul\n");
+        
+        System.out.println("CORO");
+        System.out.println("Tu bandera es un lampo de cielo");
+        System.out.println("Por un bloque de nieve cruzado");
+        System.out.println("y se ven en su fondo sagrado");
+        System.out.println("Cinco estrellas de palido Azul");
+    }
+    
+}
