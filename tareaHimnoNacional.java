a308e467ef38feb0a7deb2fc88fa7730181140a5
Anderson Leon <115023863+leonxjass@users.noreply.github.com>
2023-07-17 18:49:16 -0600

    laboratorio 1 de intro a java terminado
    en este archivo se imprime linea por linea el himno nacional hondureño usando el comando println()

---
+++ tareaHimnoNacional.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Canal
+ */
+public class tareaHimnoNacional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // Himno nacional de Honduras
+        
+        System.out.println("CORO");
+        System.out.println("Tu bandera es un lampo de cielo");
+        System.out.println("por un bloque de nieve cruzado;");
+        System.out.println("y se ven en su fondo sagrado");
+        System.out.println("cinco estrellas de pálido azul;");
+        System.out.println("en tu emblema, que un mar rumoroso");
+        System.out.println("con sus ondas bravías escuda,");
+        System.out.println("de un volcán, tras la cima desnuda");
+        System.out.println("hay un astro de nítida luz.");
+        
+        System.out.println("I");
+        System.out.println("India virgen y hermosa dormías");
+        System.out.println("India virgen y hermosa dormías");
+        System.out.println("de tus mares al canto sonoro,");
+        System.out.println("cuando echada en tus cuencas de oro");
+        System.out.println("el audaz navegante te halló;");
+        System.out.println("y al mirar tu belleza extasiado,");
+        System.out.println("al influjo ideal de tu encanto,");
+        System.out.println("la orla azul de tu espléndido manto");
+        System.out.println("con su beso de amor consagró.");
+        
+        System.out.println("II");
+        System.out.println("De un país donde el sol se levanta,");
+        System.out.println("más allá del atlante azulado,");
+        System.out.println("aquel hombre te había soñado");
+        System.out.println("y en tu busca a la mar se lanzó.");
+        System.out.println("Cuando erguiste la pálida frente,");
+        System.out.println("en la viva ansiedad de tu anhelo,");
+        System.out.println("bajo el dombo gentil de tu cielo");
+        System.out.println("ya flotaba un extraño pendón.");
+        
+        System.out.println("III");    
+        System.out.println("Era inútil que el indio tu amado,");    
+        System.out.println("se aprestara a la lucha con ira,");    
+        System.out.println("porque envuelto en su sangre Lempira");    
+        System.out.println("en la noche profunda se hundió;");    
+        System.out.println("y de la épica hazaña, en memoria,");    
+        System.out.println("la leyenda tan sólo ha guardado");    
+        System.out.println("de un sepulcro el lugar ignorado");    
+        System.out.println("y el severo perfil de un peñón.");
+        
+        System.out.println("IV");
+        System.out.println("Por tres siglos tus hijos oyeron");
+        System.out.println("el mandato imperioso del amo;");
+        System.out.println("por tres siglos tu inútil reclamo");
+        System.out.println("en la atmósfera azul se perdió;");
+        System.out.println("pero un día gloria tu oído");
+        System.out.println("percibió, poderoso y distante");
+        System.out.println("que allá lejos, por sobre el atlante");
+        System.out.println("indignado rugía un león.");
+        
+        System.out.println("V");
+        System.out.println("Era Francia, la libre, la heroica,");
+        System.out.println("que en su sueño de siglos dormida");
+        System.out.println("despertaba iracunda a la vida");
+        System.out.println("al reclamo viril de Dantón;");
+        System.out.println("era Francia que enviaba a la muerte");
+        System.out.println("la cabeza del rey consagrado");
+        System.out.println("y que alzaba, soberbia a su lado");
+        System.out.println("el altar de la Diosa razón.");
+        
+        System.out.println("VI");
+        System.out.println("Tú también ¡Oh mi patria! te alzaste");
+        System.out.println("de tu sueño servil y profundo;");
+        System.out.println("tú también enseñastes al mundo");
+        System.out.println("destrozado el infame eslabón.");
+        System.out.println("Y en tu suelo bendito, tras la alta");
+        System.out.println("cabellera de monte salvaje");
+        System.out.println("como un ave de negro plumaje");
+        System.out.println("la colonia fugaz se perdió.");
+        
+        System.out.println("VII");
+        System.out.println("Por guardar ese emblema divino");
+        System.out.println("marcharemos ¡Oh patria! a la muerte;");
+        System.out.println("generosa será nuestra suerte");
+        System.out.println("si morimos pensando en tu amor.");
+        System.out.println("Defendiendo tu santa bandera,");
+        System.out.println("y en tus pliegues gloriosos cubiertos,");
+        System.out.println("serán muchos, Honduras, tus muertos,");
+        System.out.println("pero todos caerán con honor.");
+    }
+}
