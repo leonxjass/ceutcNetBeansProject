1c090c41b6eb9fd0074b5d2864da954f78a2fc9a
Anderson Leon <115023863+leonxjass@users.noreply.github.com>
2023-07-19 11:34:05 -0600

    laboratorio 1 tabla de productos

---
+++ laboratorio1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tguca-L01
+ */
+public class laboratorio1 {
+    
+    public static void main(String[] args){
+         /*
+        creando datos de tabla de productos    
+    */
+    
+    //declarando variables
+        String cp1, cp2, cp3, cp4, cp5;
+        String np1, np2, np3, np4, np5;
+        int c1, c2, c3, c4, c5;
+        double p1, p2, p3, p4, p5;
+        boolean g1, g2, g3, g4, g5;
+    
+    //entrada de datos
+        cp1 = "P01"; np1 = "QUESO"; c1 = 3; p1 = 47.50; g1 = false;
+        cp2 = "P02"; np2 = "PAPAS"; c2 = 2; p2 = 08.45; g2 = false;
+        //cp1 = "P03"; np1 = "QUESO"; c1 = 3; p1 = 47.50; g1 = false;
+        //cp1 = "P04"; np1 = "QUESO"; c1 = 3; p1 = 47.50; g1 = false;
+        //cp1 = "P05"; np1 = "QUESO"; c1 = 3; p1 = 47.50; g1 = false;
+    
+    //output
+        System.out.println(cp1+"\t"+np1+"\t"+c1+"\t"+p1+"\t"+"\t"+g1);
+        System.out.println(cp2+"\t"+np2+"\t"+c2+"\t"+p2+"\t"+"\t"+g2);
+    
+    }
+}
