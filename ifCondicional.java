64f508bf6f476e79b7309db321ffb34456feb025
Anderson Leon <115023863+leonxjass@users.noreply.github.com>
2023-07-24 11:28:50 -0600

    ejercicio de condicional if en clase semana 2
    realizamos un programa de edades para la clase 1 de la semana 2 con el condicional if

---
+++ ifCondicional.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc
+ */
+public class Repaso2 {
+    
+    public static void main(String[] args){
+    
+    //If and else segunda semana de clases
+        
+        int edad = 61;
+        
+        if(edad < 18){
+            System.out.println("Es menor de edad");
+        }else if(edad >= 18 && edad < 21){
+            System.out.println("Ud aun es un joven");
+        }else if(edad >= 25 && edad < 60){
+            System.out.println("ud es un adulto");
+        }else if(edad >= 60){
+            System.out.println("Ud es un adulto mayor");
+        }
+            
+    
+    
+    }
+    
+}
