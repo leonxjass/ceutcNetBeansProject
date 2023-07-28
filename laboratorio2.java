386380859d30498c1c976bb9422e5d0ccaf1023f
Anderson Leon <115023863+leonxjass@users.noreply.github.com>
2023-07-28 11:28:04 -0600

    actualizamos tabla de productos lab2 sem 2
    agregamos totales y calculos de impuestos y subtotales

--- laboratorio2.java
+++ laboratorio2.java
@@ -5,14 +5,13 @@
  */
 
 /**
- *
- * @author pc
+ * @author tguca-L01
  */
-public class Repaso2 {
-    
-    public static void main(String[] args){
-         /*
-        creando datos de tabla de productos    
+public class laboratorio2 {
+        
+  public static void main(String[] args){
+    /*
+        creando datos de tabla de productos && calculando datos    
     */
     
     //declarando variables
@@ -21,7 +20,13 @@ public class Repaso2 {
         int c1, c2, c3, c4, c5;
         double p1, p2, p3, p4, p5;
         boolean g1, g2, g3, g4, g5;
-    
+        
+        double st1, st2, st3, st4, st5;
+        double imp1, imp2, imp3, imp4, imp5;
+        double t1, t2, t3, t4, t5;
+        double tp;
+        
+     
     //entrada de datos
         cp1 = "P01"; np1 = "QUESO";     c1 = 3; p1 = 47.50; g1 = false;
         cp2 = "P02"; np2 = "PAPAS";     c2 = 2; p2 = 08.45; g2 = false;
@@ -30,24 +35,53 @@ public class Repaso2 {
         cp5 = "P05"; np5 = "CHILE(4OZ)";c5 = 3; p5 = 47.50; g5 = true;
     
     //definiendo formatos para print System.out.format
-        String f1, f2, f3, f4, f5, fg;
+        String f1, f2, f3, f4, f5, f6, f7, f8, fg;
         f1 = "%-8s";
         f2 = "%-15s";
         f3 = "%-10d";
         f4 = "%-16.2f";
-        f5 = "%-55b";
-        fg = f1+f2+f3+f4+f5+"\n";
+        f5 = "%-15b";
+        f6 = "%-17.2f";
+        f7 = "%-15.2f";
+        f8 = "%-9.2f";
+        fg = f1+f2+f3+f4+f5+f6+f7+f8+"\n";
+    
+    //Calculo de datos
+        st1 = p1 * c1; 
+        st2 = p2 * c2;
+        st3 = p3 * c3;
+        st4 = p4 * c4;
+        st5 = p5 * c5; 
         
+        imp1 = st1*0.15; 
+        imp2 = st2*0.15; 
+        imp3 = st3*0.15; 
+        imp4 = st4*0.15; 
+        imp5 = st5*0.15; 
+        
+        t1 = st1 + imp1;
+        t2 = st2 + imp2;
+        t3 = st3 + imp3;
+        t4 = st4 + imp4;
+        t5 = st5 + imp5;
+        
+        if(g1) imp1 = st1 * 0.15; else imp1 = 0;
+        if(g2) imp2 = st2 * 0.15; else imp2 = 0;
+        if(g3) imp3 = st3 * 0.15; else imp3 = 0;
+        if(g4) imp4 = st4 * 0.15; else imp4 = 0;
+        if(g5) imp5 = st5 * 0.15; else imp5 = 0;
+    
+        tp = t1+t2+t3+t4+t5;
         
     //output
         System.out.println("================================================="); 
-        System.out.println("COD\tPRODUCTO   CANTIDAD\tPRECIO\t\tGRABABILIDAD"); 
+        System.out.println("COD\tPRODUCTO   CANTIDAD\tPRECIO\t\tGRABABILIDAD"
+                + "\tSUBTOTAL\tIMPUESTO\tTOTAL"); 
         System.out.println("================================================="); 
-        System.out.format(fg,cp1,np1,c1,p1,g1); 
-        System.out.format(fg,cp2,np2,c2,p2,g2);
-        System.out.format(fg,cp3,np3,c3,p3,g3); 
-        System.out.format(fg,cp4,np4,c4,p4,g4); 
-        System.out.format(fg,cp5,np5,c5,p5,g5); 
+        System.out.format(fg,cp1,np1,c1,p1,g1,st1,imp1,t1); 
+        System.out.format(fg,cp2,np2,c2,p2,g2,st2,imp2,t2); 
+        System.out.println("================================================="); 
+        System.out.println("Total a pagar" + tp); 
         System.out.println("================================================="); 
 
         
@@ -78,6 +112,10 @@ public class Repaso2 {
             Si no hacemos el salto de linea al final de un format el output 
             de los datos, nos resultara en una sola linea horizontal, 
             es necesario el final del formato el "\n"
+         
+            laboratorio 2
+            
+            
          
         */
     
