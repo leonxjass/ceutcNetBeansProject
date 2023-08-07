7b3bbb4bab1d5f3b2ed9a0422d5e6abec4472825
Anderson Leon <leon.jass@outlook.com>
2023-08-07 11:27:41 -0600

    project calendario en java Start
    creamos la mecanica para imprimir los dias y hacer los saltos de linea respectivos, creamos tambien la mecanica para controlar que dia de la semana inicia el mes

---
+++ projectCalendarioProgra1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication600;
+
+/**
+ *
+ * @author tguca-L01
+ */
+public class projectCalendarioProgra1 {
+    
+    public static void main(String[] arg){
+        int dia = 0;
+        
+        System.out.println("Do\tLu\tMa\tMi\tJu\tVi\tSa");
+        
+        for(int c=1; c<=42; c++){
+            if(c>=6)
+                dia = dia + 1; //1
+            System.out.print(dia + "\t");
+            if(c%7 == 0)System.out.println(""); // 2
+        }       
+        //salto de linea
+        System.out.println("");//3
+    }
+}
+
+/**
+ * 2) lo utilizamos para que nuestros dias empiezen en la casilla 3
+ * 1) lo usamos para imprimir los saltos de linea
+ * para crear las casillas del calendario
+ * 3) lo usamos para que el sistema haga un salto de linea y se 
+ * creen las filas y que no lo imprima como una sola fila
+ */
