fa7c499c8f9eeff7c7e65aa382c881faf15cb1d0
Anderson Leon <leon.jass@outlook.com>
2023-08-09 11:34:58 -0600

    actualizamos proyecto
    definimos var de dias y lo complementamos con arr y ciclos

--- projectCalendarioProgra1.java
+++ projectCalendarioProgra1.java
@@ -10,29 +10,64 @@ package javaapplication600;
  * @author tguca-L01
  */
 public class projectCalendarioProgra1 {
-    
-    public static void main(String[] arg){
+   
+    public static void cal1(){
         int dia = 0;
         
         System.out.println("Do\tLu\tMa\tMi\tJu\tVi\tSa");
         
         for(int c=1; c<=42; c++){
-            if(c>=6)//4
+            if(c>=6)
                 dia = dia + 1; //1
-            if(dia > 31) break;
+            //if(dia>31) break;
             System.out.print(dia + "\t");
             if(c%7 == 0)System.out.println(""); // 2
         }       
         //salto de linea
         System.out.println("");//3
+    
+    }
+    public static void cal2(String nombreMes,int iniDiaMes, int tDiaMes){
+         
+        int dia = 0;
+        
+        System.out.println(nombreMes);
+        System.out.println("Do\tLu\tMa\tMi\tJu\tVi\tSa");
+        
+        for(int c=1; c<=42; c++){
+           if(c>iniDiaMes && dia<tDiaMes){
+               dia = dia +1;
+           System.out.print(dia + "\t");}
+           else
+                System.out.print(" " + "\t");
+           if(c%7 == 0) System.out.println("");
+        }
+        System.out.println(" ");
+    }
+    public static void main(String[] arg){
+        //cal2("Enero", 0,31);
+        String mes[] = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio"
+            ,"Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+        int ds[] = {0,3,3,6,1,4,6,2,5,0,3,5};
+        int dm[] = {0,31,28,31,30,31,30,31,31,30,30,31};
+        
+        for(int k=1; k<12; k++)
+            cal2(mes[k],ds[k],dm[k]);
     }
 }
 
 /**
+ * **NOTA: Valido para el cal1 nada mas**
  * 2) lo utilizamos para que nuestros dias empiezen en la casilla 3
  * 1) lo usamos para imprimir los saltos de linea
  * para crear las casillas del calendario
  * 3) lo usamos para que el sistema haga un salto de linea y se 
  * creen las filas y que no lo imprima como una sola fila
- * controlamos en que dia de la semana inicia el mes.
+ * 
+ **NOTA2 el cal2 es el mismo solo que retomado desde cero**
+ * aplicamos la regla de negocio que el calendario debe iniciar en el dia de la
+ * semana 5, en este caso viernes, lo logramos creando una variable que contenga
+ * el valor numerico del dia y le decimos que la casilla sume 1 si la casilla es 
+ * menor que la var iniDiames que le sume 1, detemos la iteracion diciendole
+ * que si dia es menor que el tDiaMes no siga imprimiendo
  */
